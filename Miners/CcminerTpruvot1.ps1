8efad59d0930eb07a7ca8eb44f575e5395dd7a93
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 16:16:43 -0700

    Update CcminerTpruvot1.ps1

--- Miners/CcminerTpruvot1.ps1
+++ Miners/CcminerTpruvot1.ps1
@@ -1,4 +1,4 @@
-﻿$Path = '.\Bin\NVIDIA-TPruvot2\ccminer.exe'
+﻿$Path = '.\Bin\NVIDIA-TPruvot1\ccminer.exe'
 $Uri = 'https://github.com/tpruvot/ccminer/releases/download/2.2.4-tpruvot/ccminer-x86-2.2.4-cuda9.7z'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
