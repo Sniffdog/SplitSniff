555b962ef2e37360210d57365bdeb5fa248df79f
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 16:02:48 -0700

    Update CcminerSp-modSplit.ps1

--- Miners/CcminerSp-modSplit.ps1
+++ Miners/CcminerSp-modSplit.ps1
@@ -42,10 +42,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
     [PSCustomObject]@{
         Type = "NVIDIA2"
         Path = $Path
-        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u $($Pools.(Get-Algorithm($_)).User2) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4070 -u $($Pools.(Get-Algorithm($_)).User2) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Wrapper"
-        Port = 4069 
+        Port = 4070 
         Wrap = $true
         URI = $Uri
     }
