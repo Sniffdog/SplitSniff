c0080a3076dc93cf0dee306dcd3934c401d2177b
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 10:42:46 -0600

    Add files via upload

---
+++ Miners/CcminerSp-modSplit.ps1
@@ -0,0 +1,52 @@
+. .\Include.ps1
+
+$Path = ".\Bin\NVIDIA-SP-PHI2\ccminer-phi-sp1.exe"
+$Uri = 'http://files.dangerd.org/pub/Mining//ccminer-phi-sp1.zip'
+
+$Commands = [PSCustomObject]@{
+    #"bitcore" = "" #Bitcore
+    #"blake2s" = "" #Blake2s
+    #"blakecoin" = "" #Blakecoin
+    #"c11" = "" #C11
+    #"vanilla" = "" #BlakeVanilla
+    #"cryptonight" = "" #Cryptonight
+    #"decred" = "" #Decred
+    #"equihash" = "" #Equihash
+    #"ethash" = "" #Ethash
+    #"groestl" = "" #Groestl
+    #"hmq1725" = "" #hmq1725
+    #"keccak" = "" #Keccak
+    #"lbry" = "" #Lbry
+    #"lyra2v2" = "" #Lyra2RE2
+    #"lyra2z" = "" #Lyra2z
+    #"myr-gr" = "" #MyriadGroestl
+    #"neoscrypt" = "" #NeoScrypt
+    #"nist5" = "" #Nist5
+    #"pascal" = "" #Pascal
+    #"qubit" = "" #Qubit
+    #"scrypt" = "" #Scrypt
+    #"sia" = "" #Sia
+    #"sib" = "" #Sib
+    #"skein" = "" #Skein
+    #"timetravel" = "" #Timetravel
+    #"x11" = "" #X11
+    #"x11evo" = "" #X11evo
+    #"x17" = "" #X17
+    #"yescrypt" = "" #Yescrypt
+    "phi" = " -d $SplitSniffCC2" #Phi
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA2"
+        Path = $Path
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u 1AMQg6m9GPDN9HGuC3wJGpSuiZr1XQXjxi -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
+        API = "Wrapper"
+        Port = 4069 
+        Wrap = $true
+        URI = $Uri
+    }
+}
