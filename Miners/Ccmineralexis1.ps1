96423a7d271802a4089a5bd0d2a10322dbfd62ff
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 16:19:13 -0700

    Update Ccmineralexis1.ps1

--- Miners/Ccmineralexis1.ps1
+++ Miners/Ccmineralexis1.ps1
@@ -1,4 +1,4 @@
-﻿$Path = '.\Bin\NVIDIA-Alexis2\ccminer.exe'
+﻿$Path = '.\Bin\NVIDIA-Alexis1\ccminer.exe'
 $Uri = 'https://github.com/nemosminer/ccminer-Alexis78/releases/download/ccminer-alexis78/ccminer-alexis78-ms2013-cuda7.5.7z'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
