968079c7e329a774e28bb699f4675ecb8b3f6d23
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 15:57:50 -0700

    Update and rename CcminerpalginSplit.ps1 to Ccminerpalgin1.ps1

--- Miners/Ccminerpalgin1.ps1
+++ Miners/Ccminerpalgin1.ps1
@@ -16,7 +16,7 @@ $Commands = [PSCustomObject]@{
     #"hmq1725" = "" #hmq1725
     #"keccak" = "" #Keccak
     #"lbry" = "" #Lbry
-    #"lyra2v2" = " -d $SplitSniffCC" #Lyra2RE2
+    #"lyra2v2" = " -d $SplitSniffCC1" #Lyra2RE2
     #"lyra2z" = "" #Lyra2z
     #"myr-gr" = "" #MyriadGroestl
     #"neoscrypt" = "" #NeoScrypt
@@ -30,13 +30,13 @@ $Commands = [PSCustomObject]@{
     #"timetravel" = "" #Timetravel
     #"x11" = "" #X11
     #"veltor" = "" #Veltor
-    "x11evo" = " -d $SplitSniffCC2" #X11evo
+    #"x11evo" = " -d $SplitSniffCC1" #X11evo
     #"x17" = "" #X17
     #"yescrypt" = "" #Yescrypt
     #"quark" = "" #Quark
     #"skunk" = "" #skunk
     #"c11" = "" #C11
-    "xevan" = " -d $SplitSniffCC2" #Xevan
+    "xevan" = " -d $SplitSniffCC1" #Xevan
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -45,7 +45,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA2"
         Path = $Path
-        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u $($Pools.(Get-Algorithm($_)).User2) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u $($Pools.(Get-Algorithm($_)).User1) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Ccminer"
         Port = 4069
