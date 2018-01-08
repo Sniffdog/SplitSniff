ecc1bc981675ac0006263af312db1f3cb96a3b83
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 17:15:19 -0700

    Update and rename CcminerTpruvotSplit.ps1 to CcminerTpruvot7.ps1

--- Miners/CcminerTpruvot7.ps1
+++ Miners/CcminerTpruvot7.ps1
@@ -1,4 +1,4 @@
-﻿$Path = '.\Bin\NVIDIA-TPruvot2\ccminer.exe'
+﻿$Path = '.\Bin\NVIDIA-TPruvot7\ccminer.exe'
 $Uri = 'https://github.com/tpruvot/ccminer/releases/download/2.2.4-tpruvot/ccminer-x86-2.2.4-cuda9.7z'
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -39,49 +39,49 @@ $Algorithms = [PSCustomObject]@{
 }
 
 $Optimizations = [PSCustomObject]@{
-    Lyra2z = ' -d $SplitSniffCC2'
+    Lyra2z = ' -d $SplitSniffCC7'
     Equihash = ''
-    Cryptonight = ' -d $SplitSniffCC2'
+    Cryptonight = ' -d $SplitSniffCC7'
     Ethash = ''
     Sia = ''
     Yescrypt = ''
     BlakeVanilla = ''
-    Lyra2RE2 = ' -d $SplitSniffCC2'
+    Lyra2RE2 = ' -d $SplitSniffCC7'
     Skein = ''
     Qubit = ''
     NeoScrypt = ''
     X11 = ''
     MyriadGroestl = ''
     Groestl = ''
-    Keccak = ' -d $SplitSniffCC2'
+    Keccak = ' -d $SplitSniffCC7'
     Scrypt = ''
-    Bitcore = ' -d $SplitSniffCC2'
+    Bitcore = ' -d $SplitSniffCC7'
     Blake2s = ''
     Sib = ''
     X17 = ''
     Quark = ''
-    Hmq1725 = ' -d $SplitSniffCC2'
+    Hmq1725 = ' -d $SplitSniffCC7'
     Veltor = ''
-    X11evo = ' -d $SplitSniffCC2'
-    Timetravel = ' -d $SplitSniffCC2'
+    X11evo = ' -d $SplitSniffCC7'
+    Timetravel = ' -d $SplitSniffCC7'
     Blakecoin = ''
     Lbry = ''
-    Jha = ' -d $SplitSniffCC2'
-    Skunk = ' -d $SplitSniffCC2'
-    Tribus = ' -d $SplitSniffCC2'
-    Phi = ' -d $SplitSniffCC2'
-    Hsr = ' -d $SplitSniffCC'
+    Jha = ' -d $SplitSniffCC7'
+    Skunk = ' -d $SplitSniffCC7'
+    Tribus = ' -d $SplitSniffCC7'
+    Phi = ' -d $SplitSniffCC7'
+    Hsr = ' -d $SplitSniffCC7'
     
 }
 
 $Algorithms | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type = 'NVIDIA2'
+        Type = 'NVIDIA7'
         Path = $Path
-        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -b 0.0.0.0:4069 -u $($Pools.', $_, '.User2)  -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
+        Arguments = -Join ('-a ', $Algorithms.$_, ' -o stratum+tcp://$($Pools.', $_, '.Host):$($Pools.', $_, '.Port) -b 0.0.0.0:4075 -u $($Pools.', $_, '.User7)  -p $($Pools.', $_, '.Pass)', $Optimizations.$_)
         HashRates = [PSCustomObject]@{$_ = -Join ('$($Stats.', $Name, '_', $_, '_HashRate.Week)')}
         API = 'Ccminer'
-        Port = 4069
+        Port = 4075
         Wrap = $false
         URI = $Uri
     }
