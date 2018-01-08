5c872054cccd02d715aa608b18494575c1bae7e0
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 17:04:23 -0700

    Update and rename CcminerSp-modSplit.ps1 to CcminerSp-mod6.ps1

--- Miners/CcminerSp-mod6.ps1
+++ Miners/CcminerSp-mod6.ps1
@@ -1,4 +1,4 @@
-$Path = ".\Bin\NVIDIA-SP-mod\ccminer.exe"
+$Path = ".\Bin\NVIDIA-SP-mod6\ccminer.exe"
 $Uri = "https://github.com/sp-hash/ccminer/releases/download/1.5.81/release81.7z"
 
 $Commands = [PSCustomObject]@{
@@ -31,19 +31,19 @@ $Commands = [PSCustomObject]@{
     #"x11evo" = "" #X11evo
     #"x17" = "" #X17
     #"yescrypt" = "" #Yescrypt
-    "phi" = " -d $SplitSniffCC2" #Phi
+    "phi" = " -d $SplitSniffCC6" #Phi
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
-        Type = "NVIDIA2"
+        Type = "NVIDIA6"
         Path = $Path
-        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u $($Pools.(Get-Algorithm($_)).User2) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4074 -u $($Pools.(Get-Algorithm($_)).User6) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
         API = "Wrapper"
-        Port = 4069 
+        Port = 4074 
         Wrap = $true
         URI = $Uri
     }
