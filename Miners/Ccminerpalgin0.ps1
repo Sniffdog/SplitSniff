a17a08ad65e5bda3820c240eb01e49b4abd6acc6
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 18:49:26 -0700

    Update Ccminerpalgin0.ps1

--- Miners/Ccminerpalgin0.ps1
+++ Miners/Ccminerpalgin0.ps1
@@ -43,7 +43,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
     [PSCustomObject]@{
-        Type = "NVIDIA"
+        Type = "NVIDIA0"
         Path = $Path
         Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User0) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
