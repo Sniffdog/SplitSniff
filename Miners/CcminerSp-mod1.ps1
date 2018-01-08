2494a71be321b8a71464fcb2751bb458079b4282
Sniffdog <30452757+Sniffdog@users.noreply.github.com>
2018-01-07 18:40:53 -0700

    Update CcminerSp-mod1.ps1

--- Miners/CcminerSp-mod1.ps1
+++ Miners/CcminerSp-mod1.ps1
@@ -33,14 +33,14 @@ $Commands = [PSCustomObject]@{
     #"x11evo" = "" #X11evo
     #"x17" = "" #X17
     #"yescrypt" = "" #Yescrypt
-    "phi" = " -d $SplitSniffCC1" #Phi
+    #"phi" = " -d $SplitSniffCC1" #Phi
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
-        Type = "NVIDIA2"
+        Type = "NVIDIA1"
         Path = $Path
         Arguments = "-a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -b 0.0.0.0:4069 -u $($Pools.(Get-Algorithm($_)).User1) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week}
